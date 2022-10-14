67802f76224c45269cdebd19643f161685218b2f
andreaa21 <andrea.zollini@proton.me>
2022-10-14 19:03:04 +0200

    cambiato variabile check

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -8,16 +8,16 @@
 const mailUtente = prompt('Inserisci la tua email');
 const output = document.querySelector('output');
 const emailList = ['email1', 'email2', 'email3', 'email4', 'email5']
-let check = false;
+let check = true;
 
 
 for(i = 0; i < emailList.length; i++) {
     if(emailList[i] === mailUtente){
-        check = true;
+        check = false;
     }
 }
 
-if(!check) {
+if(check) {
     output.innerHTML = 'questa mail può essere usata'
 }else{
     output.innerHTML = 'questa mail è già in uso'
